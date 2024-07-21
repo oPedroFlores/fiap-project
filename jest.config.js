0286115fb0caf41953dc484c12803608cab4e40c
Pedro Flores <pedrolucaspflores@gmail.com>
2024-07-21 17:24:13 -0300

    Correção duplicidade de testes

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.js'],
-    coverageDirectory: 'coverage',
-    coverageReporters: ['json', 'lcov', 'text', 'clover'],
-  };
